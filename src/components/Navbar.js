e5cf61b7a5f66ab91454fd0e07c2f73c0cf288e0
akshitgupta2206 <akshitgupta2206@gmail.com>
2025-08-10 07:40:33 +0000

    Web Update 8

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -47,7 +47,7 @@ const Navbar = () => {
           </div>
           <div className="social-icons">
             <a
-              href="https://www.linkedin.com/company/sunriise-papers/" // Updated LinkedIn URL (example)
+              href="https://www.linkedin.com/in/dinesh-gupta-57b513374" // Updated LinkedIn URL (example)
               target="_blank"
               rel="noopener noreferrer"
               className="social-icon-link text-muted me-3"
